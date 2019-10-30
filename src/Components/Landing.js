abb4836d7fd18a656661c9f3b8b1efa2fb8a455e
Jared Hasson <jhasson@simplifile.com>
2019-10-30 10:48:22 -0600

    created Landing Component and added twitter & facebook links

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,11 +1,39 @@
 import React, {Component} from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFacebook, faGithub, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons";
 
 class Landing extends Component {
+    constructor(props){
+        super(props);
+        this.landingDate = props.landingData;
+    }
     render() {
         return (
-            <div>
-                
-            </div>
+            <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+                <div className="w-100">
+                    <h1 className="mb-0">{this.landingData.firstName}
+                        <span className="text-primary">{this.landingData.lastName}</span>
+                    </h1>
+                    <div className="subheading mb-5">{this.landingData.phoneNumber} ·
+                        <a href="mailto:name@email.com">{this.landingData.email}</a>
+                    </div>
+                    <p className="lead mb-5">{this.landingData.bio}</p>
+                    <div className="social-icons">
+                        <a href={this.landingData.linkedin}>
+                            <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                        <a href={this.landingData.github}>
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                        <a href={this.landingData.twitter}>
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </a>
+                        <a href={this.landingData.facebook}>
+                            <FontAwesomeIcon icon={faFacebook} />
+                        </a>
+                    </div>
+                </div>
+            </section>
         );
     }
 }
